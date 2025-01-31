2838d2cf9f2f40b19e40e41d4bfd80f7427809db
Natsumi.H <natsmy.1211@gmail.com>
2025-01-31 22:40:19 +0900

    cont

---
+++ amplify/backend.ts
@@ -0,0 +1,23 @@
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
+import { helloWorld } from "./functions/hello-world/resource";
+
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ */
+const backend = defineBackend({
+  auth,
+  data,
+  helloWorld,
+});
+
+const authenticatedUserIamRole =
+  backend.auth.resources.authenticatedUserIamRole;
+backend.helloWorld.resources.lambda.grantInvoke(authenticatedUserIamRole);
+
+backend.addOutput({
+  custom: {
+    helloWorldFunctionName: backend.helloWorld.resources.lambda.functionName,
+  },
+});
